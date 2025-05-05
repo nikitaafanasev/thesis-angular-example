0dfd93e0bfab92dea9406690cc78b5e29134ad3e
Nikita Afanasev <nikita.afanasev@viadee.de>
2025-05-05 17:21:05 +0200

    edit route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,5 @@ import { PageTwoComponent } from './page-two/page-two.component';
 export const routes: Routes = [
     { path: 'page-one', component: PageOneComponent },
     { path: 'page-two', component: PageTwoComponent },
-    { path: '', component: PageOneComponent }
+    { path: '/', component: PageOneComponent }
 ];
